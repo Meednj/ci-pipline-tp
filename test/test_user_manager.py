efe408123ce16071069f78bbc939d7fb9cd493e3
Med TheG <simoplus123@gmail.com>
2026-03-12 11:45:11 +0000

    first-commit

--- test/test_user_manager.py
+++ test/test_user_manager.py
@@ -1,28 +1,28 @@
 import pytest
-from app.User_manager import UserManager
+from app.User_manager import userManager
 
 
 def test_add_user():
-    manager = UserManager()
+    manager = userManager()
     manager.add_user("adam")
     assert manager.count_users() == 1
 
 
 def test_add_existing_user():
-    manager = UserManager()
+    manager = userManager()
     manager.add_user("adam")
     with pytest.raises(ValueError):
         manager.add_user("adam")
 
 
-def test_remover_user():
-    manager = UserManager()
+def test_remove_user():
+    manager = userManager()
     manager.add_user("youssef")
     manager.remove_user("youssef")
     assert manager.count_users() == 0
 
 
 def test_remove_unknown_user():
-    manager = UserManager()
+    manager = userManager()
     with pytest.raises(ValueError):
         manager.remove_user("saad")
